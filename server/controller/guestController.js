2c1276690f2e3db8b411961662d06239abbba7ad
Maisa Basher <maisacompete@gmail.com>
2024-05-09 10:56:22 -0400

    structured backend

--- server/controller/guestController.js
+++ server/controller/guestController.js
@@ -1,14 +1,26 @@
 var guestDB = require('../model/mealpal_guest');
 
+/*
+    Parameters:
+        Name: string
+        Phone: String
+    Description: Checks if the guest data is valid 
+    Return: Json status
+*/
 
 function guestController(name, phone){
     let res = {
         status:'',
         message:''
     };
-    if(name.length < 2 || phone.length < 10 || phone.length > 11){
+    if(name.split(" ").length < 2 || phone.length < 10 || phone.length > 11){
         res.status = 400;
-        res.message = "Invalid entry"
+        res.message = "Invalid entry";
+        if(name.split(" ").length < 2)
+            res.message += " name";
+        if(phone.length < 10 || phone.length > 11)
+            res.message += " phone"; 
+            
     } else{
         guestDB.insertData(name, phone);
         res.status = 201;
--- server/server.js
+++ server/server.js
@@ -2,19 +2,16 @@ const express = require("express");
 const cors = require("cors");
 var db = require('./model/mealpal_guest');
 var guestController = require('./controller/guestController')
-
-db.createDB();
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+db.createDB();
 
+//Api Setup 
 app.post('/', (req,res)=>{
-    //db.insertData(req.body.name, req.body.phone);
     const out = guestController.guestController(req.body.name, req.body.phone);
     res.status(out.status);
-    // res.statusText(out.message);
-    console.log(res.body)
     return res.json(out.message);
 })
 
